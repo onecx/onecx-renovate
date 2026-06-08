0f6f624c646e817105b50cf389d92e4eaa6a646c
Andrej Petras <andrej@lorislab.org>
2026-06-08 12:02:01 +0200

    ci: create renovate configuration

---
+++ config/renovate/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    platform: 'github',
+    username: 'tkit-dev',
+    gitAuthor: 'Renovate <tkit-dev@1000kit.org>',
+    onboarding: false,
+    autodiscover: true,
+    autodiscoverNamespace: 'onecx',
+    autodiscoverFilter: [
+        "onecx/onecx-tenant-*",
+    ],
+}
